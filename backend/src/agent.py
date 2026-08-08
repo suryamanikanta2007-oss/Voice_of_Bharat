2f7ad7727f583fdc11cd39626cc7063b76da194a
Suryamanikanta-DA <suryamanikanta2007@gmail.com>
2026-08-08 17:34:17 +0530

    Update agent.py

--- backend/src/agent.py
+++ backend/src/agent.py
@@ -94,8 +94,8 @@ async def my_agent(ctx: JobContext):
         # A Large Language Model (LLM) is your agent's brain, processing user input and generating a response
         # See all available models at https://docs.livekit.io/agents/models/llm/
         llm=google.LLM(
-                model="gemini-3.5-flash-lite",
-            ),
+            model="gemini-3.5-flash-lite",
+        ),
         # Text-to-speech (TTS) is your agent's voice, turning the LLM's text into speech that the user can hear
         # See all available models as well as voice selections at https://docs.livekit.io/agents/models/tts/
         tts=murf.TTS(
